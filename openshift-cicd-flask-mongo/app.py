d77d0ff1a1b773ebd71bf83fe37a0bcad90d5e37
John Osborne <johnfosborneiii@gmail.com>
2017-11-08 10:21:06 -0500

    adding files

--- openshift-cicd-flask-mongo/app.py
+++ openshift-cicd-flask-mongo/app.py
@@ -2,20 +2,12 @@ import os
 
 from flask import Flask, redirect, url_for, request, render_template, json, jsonify, Response
 from pymongo import MongoClient
-from bson import json_util
+from bson import json_util, ObjectId
 
 app = Flask(__name__)
 
 client = MongoClient(os.environ['MONGO_CONNECTION_URI'],27017)
 db = client.tododb
-
-@app.route('/')
-def todo():
-
-    _items = db.tododb.find()
-    items = [item for item in _items]
-
-    return render_template('todo.html', items=items)
 
 @app.route('/gettasks')
 def gettasks():
@@ -27,17 +19,6 @@ def gettasks():
         json_docs.append(json_doc)
 
     return Response(json.dumps(json_docs), mimetype='application/json')
-
-@app.route('/new', methods=['POST'])
-def new():
-
-    item_doc = {
-        'name': request.form['name'],
-        'description': request.form['completebydate']
-    }
-    db.tododb.insert_one(item_doc)
-
-    return redirect(url_for('todo'))
 
 @app.route('/addtask', methods=['POST'])
 def addtask():

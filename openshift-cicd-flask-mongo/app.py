141469358eb794b4582aa2b2f222c427e3ab6ed2
John Osborne <johnfosborneiii@gmail.com>
2017-11-26 16:23:22 -0500

    adding ability to read files (from secrets)

--- openshift-cicd-flask-mongo/app.py
+++ openshift-cicd-flask-mongo/app.py
@@ -10,38 +10,32 @@ import configparser
 
 app = Flask(__name__)
 
-def init():
-    LOG_FILENAME = '/dev/termination-log'
-    logging.basicConfig(
-        filename=LOG_FILENAME,
-        level=logging.DEBUG,
-    )
+mongoclient_uri_str = ""
 
-    mongoclient_uri_str = ""
+if os.path.isfile('/opt/app-root/etc/mongodb_user') and os.path.isfile('/opt/app-root/etc/mongodb_password') and os.path.isfile('/opt/app-root/etc/mongodb_hostname') and os.path.isfile('/opt/app-root/etc/mongodb_database'):
+    mongodb_user = open('/opt/app-root/etc/mongodb_user').readline().rstrip()
+    mongodb_password = open('/opt/app-root/etc/mongodb_password').readline().rstrip()
+    mongodb_hostname = open('/opt/app-root/etc/mongodb_hostname').readline().rstrip()
+    mongodb_database = open('/opt/app-root/etc/mongodb_database').readline().rstrip()
+    print("Using files in /opt/app-root/etc/ for MongoDB connection")
+    mongoclient_uri_str = "mongodb://"+mongodb_user+":"+mongodb_password+"@"+mongodb_hostname+"/"+mongodb_database
 
-    if os.path.isfile('/opt/app-root/etc/mongodb_user') and os.path.isfile('/opt/app-root/etc/mongodb_password') and os.path.isfile('/opt/app-root/etc/mongodb_hostname') and os.path.isfile('/opt/app-root/etc/mongodb_database'):
-        mongodb_user = open('/opt/app-root/etc/mongodb_user').readline().rstrip()
-        mongodb_password = open('/opt/app-root/etc/mongodb_password').readline().rstrip()
-        mongodb_hostname = open('/opt/app-root/etc/mongodb_hostname').readline().rstrip()
-        mongodb_database = open('/opt/app-root/etc/mongodb_database').readline().rstrip()
-        print("Using files in /opt/app-root/etc/ for MongoDB connection")
-        mongoclient_uri_str = "mongodb://"+mongodb_user+":"+mongodb_password+"@"+mongodb_hostname+"/"+mongodb_database
+elif os.environ.get('MONGO_CONNECTION_URI') is not None:
+    print("Using environment variable MONGO_CONNECTION_URI for MongoDB connection")
+    mongoclient_uri_str = os.environ['MONGO_CONNECTION_URI']
 
-    elif os.environ.get('MONGO_CONNECTION_URI') is not None:
-        print("Using environment variable MONGO_CONNECTION_URI for MongoDB connection")
-        mongoclient_uri_str = os.environ['MONGO_CONNECTION_URI']
+else:
+    with open('/dev/termination-log', 'w') as file:
+        file.write('This application requires a connection to a database, with credentials provided either:\n1. The following files available in /opt/app-root/etc/: mongodb_user, mongodb_password, mongodb_hostname, mongodb_database\n2. Environment variable MONGO_CONNECTION_URI with the credentials to pass to the MongoClient library')
+    exit()
 
-    else:
-        logging.error('This application requires a connection to a database, with credentials provided either:\n1. The following files available in /opt/app-root/etc/: mongodb_user, mongodb_password, mongodb_hostname, mongodb_database\n2. Environment variable MONGO_CONNECTION_URI with the credentials to pass to the MongoClient library')
-        exit()
-
-    try:
-        client = MongoClient(mongoclient_uri_str, 27017)
-        db = client.tododb
-        print("Successful connection to MongoDB instance")
-    except:
-        print("Error connecting to MongoDB with files")
-        exit()
+try:
+    client = MongoClient(mongoclient_uri_str, 27017)
+    db = client.tododb
+    print("Successful connection to MongoDB instance")
+except:
+    print("Error connecting to MongoDB with files")
+    exit()
 
 @app.route('/')
 def todo():
@@ -94,6 +88,5 @@ def deletetask():
     return Response(json_util.dumps({"_id": ObjectId(oid)}), 200, {'ContentType':'application/json'})
 
 if __name__ == "__main__":
-    global db = None
-    init()
-    app.run(host='0.0.0.0', debug=True)
+    #Setting use_reloader=False - https://stackoverflow.com/questions/25504149/why-does-running-the-flask-dev-server-run-itself-twice
+    app.run(host='0.0.0.0', debug=True, use_reloader=False)
